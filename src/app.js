3d1ce2b0d050db85fd57d54e58c6042a8ebc2d84
DurlabhDewangan <durlabhdewangan59@gmail.com>
2025-11-30 20:19:42 +0530

    cors optimize

--- src/app.js
+++ src/app.js
@@ -10,8 +10,11 @@ const app = express()
 
 app.use(cors({
   origin: process.env.CORS_ORIGIN, 
-  credentials: true
+  credentials: true,
+   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }));
+
 
 
 app.use(express.json({limit:"16kb"}))
